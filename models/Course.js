066e81ce7c91bf87e56bb14ac678dde6bad1bc93
Brad Traversy <traversymedia@gmail.com>
2019-10-01 18:19:57 -0400

    Course model & seeding

---
+++ models/Course.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const CourseSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    trim: true,
+    required: [true, 'Please add a course title']
+  },
+  description: {
+    type: String,
+    required: [true, 'Please add a description']
+  },
+  weeks: {
+    type: String,
+    required: [true, 'Please add number of weeks']
+  },
+  tuition: {
+    type: Number,
+    required: [true, 'Please add a tuition cost']
+  },
+  minimumSkill: {
+    type: String,
+    required: [true, 'Please add a minimum skill'],
+    enum: ['beginner', 'intermediate', 'advanced']
+  },
+  scholarshipAvailable: {
+    type: Boolean,
+    default: false
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  bootcamp: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'Bootcamp',
+    required: true
+  }
+});
+
+module.exports = mongoose.model('Course', CourseSchema);
--- seeder.js
+++ seeder.js
@@ -8,6 +8,7 @@ dotenv.config({ path: './config/config.env' });
 
 // Load models
 const Bootcamp = require('./models/Bootcamp');
+const Course = require('./models/Course');
 
 // Connect to DB
 mongoose.connect(process.env.MONGO_URI, {
@@ -22,10 +23,15 @@ const bootcamps = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8')
 );
 
+const courses = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8')
+);
+
 // Import into DB
 const importData = async () => {
   try {
     await Bootcamp.create(bootcamps);
+    await Course.create(courses);
     console.log('Data Imported...'.green.inverse);
     process.exit();
   } catch (err) {
@@ -37,6 +43,7 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Bootcamp.deleteMany();
+    await Course.deleteMany();
     console.log('Data Destroyed...'.red.inverse);
     process.exit();
   } catch (err) {
