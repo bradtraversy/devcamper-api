0bcf7b929048edef27b65d0c1108b4b09f80e7cd
Brad Traversy <traversymedia@gmail.com>
2019-10-01 13:44:01 -0400

    Advanced filtering

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -7,7 +7,15 @@ const Bootcamp = require('../models/Bootcamp');
 // @route     GET /api/v1/bootcamps
 // @access    Public
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
-  const bootcamps = await Bootcamp.find();
+  let query;
+
+  let queryStr = JSON.stringify(req.query);
+
+  queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
+
+  query = Bootcamp.find(JSON.parse(queryStr));
+
+  const bootcamps = await query;
 
   res
     .status(200)
