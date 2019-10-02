3a2d824bbcde00ef44bcb0ddd4d6438f5748c3bf
Brad Traversy <traversymedia@gmail.com>
2019-10-02 14:05:01 -0400

    Average cost calculation

--- models/Course.js
+++ models/Course.js
@@ -38,4 +38,39 @@ const CourseSchema = new mongoose.Schema({
   }
 });
 
+// Static method to get avg of course tuitions
+CourseSchema.statics.getAverageCost = async function(bootcampId) {
+  console.log('Calculating avg cost...'.blue);
+
+  const obj = await this.aggregate([
+    {
+      $match: { bootcamp: bootcampId }
+    },
+    {
+      $group: {
+        _id: '$bootcamp',
+        averageCost: { $avg: '$tuition' }
+      }
+    }
+  ]);
+
+  try {
+    await this.model('Bootcamp').findByIdAndUpdate(bootcampId, {
+      averageCost: Math.ceil(obj[0].averageCost / 10) * 10
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+// Call getAverageCost after save
+CourseSchema.post('save', function() {
+  this.constructor.getAverageCost(this.bootcamp);
+});
+
+// Call getAverageCost before remove
+CourseSchema.pre('remove', function() {
+  this.constructor.getAverageCost(this.bootcamp);
+});
+
 module.exports = mongoose.model('Course', CourseSchema);
--- routes/courses.js
+++ routes/courses.js
@@ -1,8 +1,23 @@
 const express = require('express');
-const { getCourses } = require('../controllers/courses');
+const {
+  getCourses,
+  getCourse,
+  addCourse,
+  updateCourse,
+  deleteCourse
+} = require('../controllers/courses');
 
 const router = express.Router({ mergeParams: true });
 
-router.route('/').get(getCourses);
+router
+  .route('/')
+  .get(getCourses)
+  .post(addCourse);
+
+router
+  .route('/:id')
+  .get(getCourse)
+  .put(updateCourse)
+  .delete(deleteCourse);
 
 module.exports = router;
