0bcaf49efc7d51d3d882e6e5db5028244f2cea3b
Brad Traversy <traversymedia@gmail.com>
2019-10-04 18:14:05 -0400

    Bootcamp & courses ownership

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -150,6 +150,16 @@ exports.bootcampPhotoUpload = asyncHandler(async (req, res, next) => {
     );
   }
 
+  // Make sure user is bootcamp owner
+  if (bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(
+      new ErrorResponse(
+        `User ${req.params.id} is not authorized to update this bootcamp`,
+        401
+      )
+    );
+  }
+
   if (!req.files) {
     return next(new ErrorResponse(`Please upload a file`, 400));
   }
