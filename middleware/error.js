7feb7f626c20daed06d2625ec565deb50057fff1
Brad Traversy <traversymedia@gmail.com>
2019-10-11 13:46:56 -0400

    Changed castError message

--- middleware/error.js
+++ middleware/error.js
@@ -10,7 +10,7 @@ const errorHandler = (err, req, res, next) => {
 
   // Mongoose bad ObjectId
   if (err.name === 'CastError') {
-    const message = `Resource not found with id of ${err.value}`;
+    const message = `Resource not found`;
     error = new ErrorResponse(message, 404);
   }
 
