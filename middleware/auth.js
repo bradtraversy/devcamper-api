2b22fbc81fac3c7424bdfef6ba50ad820dbfe2e0
Brad Traversy <traversymedia@gmail.com>
2019-10-11 13:46:38 -0400

    Token cookie

--- middleware/auth.js
+++ middleware/auth.js
@@ -11,29 +11,28 @@ exports.protect = asyncHandler(async (req, res, next) => {
     req.headers.authorization &&
     req.headers.authorization.startsWith('Bearer')
   ) {
+    // Set token from Bearer token in header
     token = req.headers.authorization.split(' ')[1];
+    // Set token from cookie
   }
-
-  // else if(req.cookies.token) {
-  //   token = req.cookies.token
+  // else if (req.cookies.token) {
+  //   token = req.cookies.token;
   // }
 
   // Make sure token exists
   if (!token) {
-    return next(new ErrorResponse('Not authorize to access this route', 401));
+    return next(new ErrorResponse('Not authorized to access this route', 401));
   }
 
   try {
     // Verify token
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-    console.log(decoded);
-
     req.user = await User.findById(decoded.id);
 
     next();
   } catch (err) {
-    return next(new ErrorResponse('Not authorize to access this route', 401));
+    return next(new ErrorResponse('Not authorized to access this route', 401));
   }
 });
 
