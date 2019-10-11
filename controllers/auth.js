644267217dd59df0b00ae4d6ced66afd03d6b2c8
Brad Traversy <traversymedia@gmail.com>
2019-10-11 13:47:28 -0400

    Added logout to clear cookie

--- controllers/auth.js
+++ controllers/auth.js
@@ -49,6 +49,21 @@ exports.login = asyncHandler(async (req, res, next) => {
   sendTokenResponse(user, 200, res);
 });
 
+// @desc      Log user out / clear cookie
+// @route     GET /api/v1/auth/logout
+// @access    Private
+exports.logout = asyncHandler(async (req, res, next) => {
+  res.cookie('token', 'none', {
+    expires: new Date(Date.now() + 10 * 1000),
+    httpOnly: true
+  });
+
+  res.status(200).json({
+    success: true,
+    data: {}
+  });
+});
+
 // @desc      Get current logged in user
 // @route     POST /api/v1/auth/me
 // @access    Private
