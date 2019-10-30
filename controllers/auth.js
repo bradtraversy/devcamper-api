51864967b028b0c053e2a9f3582d32fcbf9487e0
Brad Traversy <traversymedia@gmail.com>
2019-10-30 14:11:30 -0400

    Removed extra response

--- controllers/auth.js
+++ controllers/auth.js
@@ -152,11 +152,6 @@ exports.forgotPassword = asyncHandler(async (req, res, next) => {
 
     return next(new ErrorResponse('Email could not be sent', 500));
   }
-
-  res.status(200).json({
-    success: true,
-    data: user
-  });
 });
 
 // @desc      Reset password
