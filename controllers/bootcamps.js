124bc2efb0ac6f4f0b81a9f9b4ea54a7e872c62b
Sam <sammyiboi@gmail.com>
2020-01-05 01:13:24 -0800

    Correct Mongoose update method name

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -76,7 +76,7 @@ exports.updateBootcamp = asyncHandler(async (req, res, next) => {
     );
   }
 
-  bootcamp = await Bootcamp.findOneAndUpdate(req.params.id, req.body, {
+  bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
     runValidators: true
   });
