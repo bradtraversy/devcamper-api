9c3f9a1f98f06a0186a37cd338c74306500cca50
Sam <sammyiboi@gmail.com>
2020-05-26 21:06:06 -0700

    Fix comment in auth controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -65,7 +65,7 @@ exports.logout = asyncHandler(async (req, res, next) => {
 });
 
 // @desc      Get current logged in user
-// @route     POST /api/v1/auth/me
+// @route     GET /api/v1/auth/me
 // @access    Private
 exports.getMe = asyncHandler(async (req, res, next) => {
   const user = await User.findById(req.user.id);
